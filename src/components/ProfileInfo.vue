8c03065ec2d41848e7bdd26745ac1a222777a1e9
Robert <robert_zibert@hotmail.com>
2018-11-10 20:15:47 -0300

    final details on user profile and removing old files

--- src/components/ProfileInfo.vue
+++ src/components/ProfileInfo.vue
@@ -4,7 +4,8 @@
       class="initials"
     >{{`${userProfile.name} ${userProfile.surname}`.split(" ").map((n)=>n[0]).join("")}}</div>
     <div style="margin-top: 20px;">
-      <h3 style="color: #fff;">{{userProfile.name}} {{userProfile.surname}}</h3>
+      <h3 v-if="userProfile.name" style="color: #fff;">{{userProfile.name}} {{userProfile.surname}}</h3>
+      <h3 v-if="!userProfile.name" style="color: #fff;">{{userProfile.email}}</h3>
     </div>
     <div style="text-align: center;">
       <QBtn @click.native="toProfile()" class="btn-sm btn-primary-inverted">{{ $t('Edit Profile') }}</QBtn>
@@ -33,6 +34,16 @@ export default {
       transform({ data }) {
         return data;
       }
+    }
+  },
+  methods: {
+    toProfile() {
+      this.$router.push({
+        name: 'pageManager',
+        params: {
+          path: 'user-profile'
+        }
+      });
     }
   }
 };
